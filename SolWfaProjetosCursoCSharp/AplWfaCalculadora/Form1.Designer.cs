ef52b7b4f5c2bbcea51d1fdf97d8617355e1d9b1
Samuel de Souza <samuelsouzarosajr@gmail.com>
2021-03-14 15:10:57 -0300

    Commit da Aula #65 - Projeto Calculadora pt. 1

---
+++ SolWfaProjetosCursoCSharp/AplWfaCalculadora/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace AplWfaCalculadora
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAlgoritmo = new System.Windows.Forms.Label();
+            this.txtOperacao = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAlgoritmo
+            // 
+            this.lblAlgoritmo.AutoSize = true;
+            this.lblAlgoritmo.Location = new System.Drawing.Point(23, 36);
+            this.lblAlgoritmo.Name = "lblAlgoritmo";
+            this.lblAlgoritmo.Size = new System.Drawing.Size(103, 13);
+            this.lblAlgoritmo.TabIndex = 3;
+            this.lblAlgoritmo.Text = "Escreva o algoritmo:";
+            // 
+            // txtOperacao
+            // 
+            this.txtOperacao.Location = new System.Drawing.Point(26, 56);
+            this.txtOperacao.MaxLength = 30;
+            this.txtOperacao.Name = "txtOperacao";
+            this.txtOperacao.Size = new System.Drawing.Size(199, 20);
+            this.txtOperacao.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(26, 87);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(93, 32);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(132, 87);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(93, 32);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 150);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtOperacao);
+            this.Controls.Add(this.lblAlgoritmo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Projeto - Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAlgoritmo;
+        private System.Windows.Forms.TextBox txtOperacao;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ SolWfaProjetosCursoCSharp/AplWfaCalculadora/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplWfaCalculadora
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            // FECHAR A APLICAÇÃO
+            Application.Exit();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            // EXECUTA O CALCULO DA OPERACAO
+
+            int parcela1;
+            int parcela2;
+            string op;
+
+            string algoritmo = txtOperacao.Text;
+
+            //123+456
+
+            string sinais = "+-*/";
+
+            int index = 0;
+            foreach(char c in algoritmo)
+            {
+                foreach(char s in sinais)
+                {
+                    if(c == s)
+                    {
+                        // DEFINE OS 3 ELEMENTOS DA OPERACAO
+                        // PARCELA 1
+                        parcela1 = int.Parse(algoritmo.Substring(0, index));
+
+                        // SINAL
+                        op = c.ToString();
+
+                        // PARCELA 2
+                        parcela2 = int.Parse(algoritmo.Substring(index+1));
+                    }
+                }
+                index++;
+            }
+        }
+    }
+}
